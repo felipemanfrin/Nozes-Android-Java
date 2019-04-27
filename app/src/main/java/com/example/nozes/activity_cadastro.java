32d9e9c363eeb985e0f26c3db7bd5030d1bf6571
felipemanfrin@gmail.com <felipemanfrin@gmail.com>
2019-04-27 15:55:48 -0300

    Comecei a fazer a tela de cadastro para o pessoal do RH acessar

---
+++ app/src/main/java/com/example/nozes/activity_cadastro.java
@@ -0,0 +1,13 @@
+package com.example.nozes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
