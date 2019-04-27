c4d34d6b2d8b28738ad6984d6ef756e2b187f900
ThazSobral <thazsobral@gmail.com>
2019-04-27 13:09:36 -0300

    Second Commit
    Signed-off-by: ThazSobral <thazsobral@gmail.com>

--- app/src/main/java/com/example/nozes/MainActivity.java
+++ app/src/main/java/com/example/nozes/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.nozes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        log = (Button) findViewById(R.id.btnLogin);
+        log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logar();
+            }
+        });
+    }
+    private void logar(){
+        startActivity(new Intent(MainActivity.this, InicialActivity.class));
     }
 }
