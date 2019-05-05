dcc3e0bfe211ed9c4afc88cf7f48d449742f66c0
ThazSobral <thazsobral@gmail.com>
2019-05-05 00:42:24 -0300

    Novo Layout do projeto

---
+++ app/src/main/java/com/example/ehnozes/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.ehnozes;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_person:
+                    mTextMessage.setText(R.string.title_person);
+                    return true;
+                case R.id.navigation_tasks:
+                    mTextMessage.setText(R.string.title_tasks);
+                    return true;
+                case R.id.navigation_meetings:
+                    mTextMessage.setText(R.string.title_meetings);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
